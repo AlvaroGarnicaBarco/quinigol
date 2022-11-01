9696fca1facf908b3f36062b1d64f3a9302b32b4
Alvaro <alvarogarnica1997@gmail.com>
2022-11-01 02:01:26 +0100

    added parser parameter to bs4 and sleep time to avoid errors

--- src/data/get_data.py
+++ src/data/get_data.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+import time
 
 
 class ProbsScraper:
@@ -11,6 +12,7 @@ class ProbsScraper:
         options.add_argument('headless')
         self.driver = webdriver.Chrome(service=Service(driver_path), options=options)
         self.driver.get('https://www.eduardolosilla.es/quinigol/ayudas/orden')
+        time.sleep(1)
         
     def cambiar_tabla(self, jornada, tipo_prob):
         tipo_probs_map = {'EL': 0, 'real': 1, 'LAE': 2}
@@ -21,8 +23,8 @@ class ProbsScraper:
         jornadas.find_element(By.XPATH, f"//option[@title='{jornada}']").click()
         
     def extraer_datos(self):
-        # time.sleep(0.5)
-        soup = BeautifulSoup(self.driver.page_source)
+        time.sleep(1)
+        soup = BeautifulSoup(self.driver.page_source, 'xml')
         base = soup.find('body').find('app-orden-quinigol').find('div', {'c-listado-orden__datos__tabla-porcentajes'})
         partidos = base.find_all('div', {'class': 'c-tabla-porcentajes-quinigol__datos u-clearfix ng-star-inserted'})
         my_dict = {
